9fe1090050cf39a8011662fd1dd41e6e68480c23
sumit <sumit.bansal@loconav.com>
2018-05-18 12:47:25 +0530

    account activation done

--- app/models/user.rb
+++ app/models/user.rb
@@ -39,6 +39,16 @@ class User < ApplicationRecord
     BCrypt::Password.new(digest).is_password?(token)
   end
 
+  # Activates an account.
+  def activate
+    update_columns(activated: FILL_IN, activated_at: FILL_IN)
+  end
+
+  # Sends activation email.
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
+  end
+
   private
   # Converts email to all lower-case.
     def downcase_email
